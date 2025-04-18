7b7369548f67e76eb3fcae6fd7e6bd9582cbaf84
Niket1997 <aniket.mahangare@uber.com>
2025-04-18 22:56:34 +0530

    add embedding code

---
+++ 1/embedding.py
@@ -0,0 +1,16 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+client = OpenAI()
+
+text = "The Eiffel Tower, Paris's iconic iron lattice masterpiece, stands as a timeless symbol of French elegance and architectural brilliance."
+
+response = client.embeddings.create(
+    model="text-embedding-3-small",
+    input=text
+)
+
+print("vector embedding: ", response.data[0].embedding)
