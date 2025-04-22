54f45aa3690d0569b46eb31a48b1fe473ada76a8
Niket1997 <aniket.mahangare@uber.com>
2025-04-22 20:17:54 +0530

    added simple chat from openai & gemini

---
+++ 2_hello_world_master_prompting/chat_openai.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+client = OpenAI()
+
+response = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[{"role": "user", "content": "What is 2+2*0?"}],
+)
+
+print(response.choices[0].message.content)
