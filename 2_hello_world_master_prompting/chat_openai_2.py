ca9e36f5fde0e529be8fc1ca34f190634945d056
Niket1997 <aniket.mahangare@uber.com>
2025-04-22 21:37:15 +0530

    add chain of thought prompting

---
+++ 2_hello_world_master_prompting/chat_openai_2.py
@@ -0,0 +1,45 @@
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+client = OpenAI()
+
+### few shot prompting
+system_prompt = """
+Your name is Nik. 
+You are a helpful assistant who is specialized in Maths. 
+You should not answer any query that is not related to Maths. You can be creative with your answers.
+
+For a given query help user to solve that along with exlanation. 
+
+Example:
+Input: 2 + 2
+Output: 2 + 2 is 4 which is calculated by adding 2 and 2.
+
+Input: 3 * 10
+Output: 3 * 10 is 30 which is calculated by multiplying 3 and 10. Fun fact, you can even multiply 10 with 3 which will also give you 30.
+
+Input: Why is sky blue?
+Output: Bruh? You alright? Is it maths query?
+"""
+
+response = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": "What is 5 * 45?"},
+    ], # zero shot prompting, as no expectations are set
+)
+
+print(response.choices[0].message.content)
+
+response = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": "What is a mobile phone?"},
+    ], # zero shot prompting, as no expectations are set
+)
+
+print(response.choices[0].message.content)
---
+++ 2_hello_world_master_prompting/chat_openai_3.py
@@ -0,0 +1,55 @@
+import json
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+client = OpenAI()
+
+# chain of thought prompting
+system_prompt = """
+You are an AI assistant who is expert in breaking down complex problems and then resolve the user's query.
+
+For the given user input, analyse the input & break down the problem into smaller sub-problems.
+At least think 5-6 steps on how to solve the problem before responding to the user.
+
+Once you get the user input, you should follow the below steps:
+1. analyse the user's input.
+2. think multiple times on how to solve the problem.
+3. come up with the output. 
+4. validate the output with the user's input.
+5. if the output is not as per the user's input, then think again and come up with the correct output.
+6. if the output is as per the user's input, then respond to the user with the output.
+
+Rules:
+1. Follow the strict JSON output as per Output schema.
+2. Always perform one step at a time & wait for next input. 
+3. Carefully analyse the user query. 
+
+Output Format:
+{{ step:"string", content:"string"}}
+
+Example:
+Input: What is 2 + 2.
+Output: {{ step:"analyse", content:"Alright! The user is interested in maths query and is asking a basic arithmetic question."}}
+Output: {{ step:"think", content:"To perform addition, I must go from left to right & add all the operands."}}
+Output: {{ step:"output", content:"The answer to 2 + 2 is 4."}}
+Output: {{ step:"validate", content:"Seems like 4 is correct answer for 2 + 2."}}
+Output: {{ step:"result", content:"2 + 2 = 4 and that is calculated by adding 2 and 2."}}
+"""
+
+response = client.chat.completions.create(
+    model="gpt-4o-mini",
+    response_format={"type": "json_object"},
+    messages=[
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": "What is 3 + 4 * 5?"},
+        {"role": "assistant", "content": json.dumps({ "step": "analyse", "content": "The user is asking a mathematics question involving both addition and multiplication, which requires understanding the order of operations." })},
+        {"role": "assistant", "content": json.dumps({"step": "think", "content": "To solve the expression 3 + 4 * 5, I need to follow the order of operations (PEMDAS/BODMAS): first evaluating the multiplication before the addition."})},
+        {"role": "assistant", "content": json.dumps({"step": "output", "content": "First, I will calculate 4 * 5, which equals 20. Then, I will add 3 to that result."})},
+        {"role": "assistant", "content": json.dumps({"step": "validate", "content": "The result of 3 + 20, where 20 comes from the multiplication, needs to be calculated to confirm accuracy."})},
+        {"role": "assistant", "content": json.dumps({"step": "result", "content": "3 + 4 * 5 = 3 + 20 = 23, which follows the correct order of operations."})}
+    ]
+)
+
+print(response.choices[0].message.content)
