aea07da44ea7c7bc1a42b3ac983e5cf7e478a53a
Niket1997 <aniket.mahangare@uber.com>
2025-05-04 23:02:29 +0530

    added checkpointer using mongodb

--- 11_langgraph_checkpointing/graph/factory.py
+++ 11_langgraph_checkpointing/graph/factory.py
@@ -1,20 +1,25 @@
+from typing import Annotated, TypedDict
 from typing import Annotated, List, Optional
 from langchain_core.language_models import BaseChatModel
 from langgraph.graph import END, START, StateGraph
 from langgraph.graph.message import add_messages
-
+from langgraph.checkpoint.mongodb import MongoDBSaver
 from models.schemas import OutputSchema
 from graph.nodes import chatbot, determine_flow, handle_tool_call
 
 
-class State(dict):
+# Define the state type
+class State(TypedDict):
     """Graph state definition."""
 
     messages: Annotated[List, add_messages]
     llm_output: Optional[OutputSchema] = None
 
 
-def create_graph(llm_with_tools: BaseChatModel) -> StateGraph:
+# Create the graph
+def create_graph(
+    llm_with_tools: BaseChatModel, checkpointer: MongoDBSaver
+) -> StateGraph:
     """Create and configure the graph.
 
     Args:
@@ -36,4 +41,4 @@ def create_graph(llm_with_tools: BaseChatModel) -> StateGraph:
     )
     graph.add_edge("tools", "chatbot")
 
-    return graph.compile()
+    return graph.compile(checkpointer=checkpointer)
--- 11_langgraph_checkpointing/main.py
+++ 11_langgraph_checkpointing/main.py
@@ -1,7 +1,8 @@
 import json
+import os
 from dotenv import load_dotenv
 from langchain.chat_models import init_chat_model
-
+from langgraph.checkpoint.mongodb import MongoDBSaver
 from models.schemas import OutputSchema
 from graph.nodes import tools
 from graph.factory import create_graph
@@ -17,31 +18,37 @@ llm_with_tools = llm.bind_tools(tools, tool_choice="auto").with_structured_outpu
     OutputSchema
 )
 
+MONGODB_URI = os.getenv("MONGODB_URI")
+config = {"configurable": {"thread_id": "1"}}
+
 
 def main():
     """Main function to run the chatbot."""
-    graph = create_graph(llm_with_tools)
+    with MongoDBSaver.from_conn_string(MONGODB_URI) as checkpointer:
+        graph = create_graph(llm_with_tools, checkpointer)
 
-    while True:
-        query = input("> ")
-        messages = [
-            {"role": "user", "content": query},
-            {"role": "system", "content": SYSTEM_PROMPT},
-        ]
+        while True:
+            query = input("> ")
+            messages = [
+                {"role": "user", "content": query},
+                {"role": "system", "content": SYSTEM_PROMPT},
+            ]
 
-        for event in graph.stream({"messages": messages, "llm_output": None}):
-            if "chatbot" in event and "llm_output" in event["chatbot"]:
-                llm_output = event["chatbot"]["llm_output"]
-                if llm_output:
-                    try:
-                        if llm_output.step == "output":
-                            print(f"🤖: {llm_output.content}")
-                        elif llm_output.step in ["action", "observe"]:
-                            continue
-                        else:
-                            print(f"🌀 {llm_output.content}")
-                    except Exception as e:
-                        print("error", e)
+            for event in graph.stream(
+                {"messages": messages, "llm_output": None}, config
+            ):
+                if "chatbot" in event and "llm_output" in event["chatbot"]:
+                    llm_output = event["chatbot"]["llm_output"]
+                    if llm_output:
+                        try:
+                            if llm_output.step == "output":
+                                print(f"🤖: {llm_output.content}")
+                            elif llm_output.step in ["action", "observe"]:
+                                continue
+                            else:
+                                print(f"🌀 {llm_output.content}")
+                        except Exception as e:
+                            print("error", e)
 
 
 if __name__ == "__main__":
