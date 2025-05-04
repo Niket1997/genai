fa0be49fb58bb1de0a4cc3cc365351c0656bb2e3
Niket1997 <aniket.mahangare@uber.com>
2025-05-04 23:23:37 +0530

    added checkpointer using mongodb

--- 11_langgraph_checkpointing/tools/command.py
+++ 11_langgraph_checkpointing/tools/command.py
@@ -4,7 +4,6 @@ from langchain_core.tools import InjectedToolCallId, tool
 from langsmith import traceable
 
 from tools.weather import generate_tool_response
-from langgraph.types import interrupt
 
 
 @tool
@@ -20,29 +19,9 @@ def execute_command(
         tool_call_id: The id of the tool call
     """
     print(f"🧰 Tool: Executing command {command}")
-    human_response = interrupt({"query": f"Execute the command: {command}"})
-    
-    # Analyze the response from interrupt
-    if human_response and isinstance(human_response, dict):
-        response_text = human_response.get("response", "").lower().strip()
-        if response_text in ["yes", "y"]:
-            response = os.system(command)
-            return generate_tool_response(
-                response=f"The command {command} returned {response}.",
-                tool_name="execute_command",
-                tool_input=command,
-                tool_call_id=tool_call_id,
-            )
-        elif response_text in ["no", "n"]:
-            return generate_tool_response(
-                response=f"Command execution cancelled by user.",
-                tool_name="execute_command",
-                tool_input=command,
-                tool_call_id=tool_call_id,
-            )
-    
+    response = os.system(command)
     return generate_tool_response(
-        response=f"The command {command} was interrupted.",
+        response=f"The command {command} returned {response}.",
         tool_name="execute_command",
         tool_input=command,
         tool_call_id=tool_call_id,
